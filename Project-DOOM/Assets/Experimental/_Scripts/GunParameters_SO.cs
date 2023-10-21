68a6d6ef0eb056dcef94e12120599e7441e94494
MadMoth125 <madmoth125@gmail.com>
2023-10-21 01:41:39 -0500

    Update GunParameters_SO.cs

--- Project-DOOM/Assets/Experimental/_Scripts/GunParameters_SO.cs
+++ Project-DOOM/Assets/Experimental/_Scripts/GunParameters_SO.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
 [CreateAssetMenu]
@@ -14,28 +15,30 @@ public class GunParameters_SO : ScriptableObject
 	[field: SerializeField]
 	public BulletType ProjectileMethod { get; private set; } = BulletType.Hitscan;
 	
-	public void Fire()
+	public void Fire(KeyCode key = KeyCode.Mouse0)
 	{
 		switch (TriggerMethod)
 		{
+			default:
 			case TriggerType.Single:
-				if (Input.GetMouseButtonDown(0)) OnGunFired?.Invoke();
+				if (Input.GetKeyDown(key)) OnGunFired?.Invoke();
 				break;
 			case TriggerType.Automatic:
-				if (Input.GetMouseButton(0)) OnGunFired?.Invoke();
+				if (Input.GetKey(key)) OnGunFired?.Invoke();
 				break;
 		}
 	}
 	
-	public void Fire(bool[] conditions)
+	public void Fire(bool[] conditions, KeyCode key = KeyCode.Mouse0)
 	{
 		switch (TriggerMethod)
 		{
+			default:
 			case TriggerType.Single:
-				if (Input.GetMouseButtonDown(0) && CheckConditionals(conditions)) OnGunFired?.Invoke();
+				if (Input.GetKeyDown(key) && CheckConditionals(conditions)) OnGunFired?.Invoke();
 				break;
 			case TriggerType.Automatic:
-				if (Input.GetMouseButton(0) && CheckConditionals(conditions)) OnGunFired?.Invoke();
+				if (Input.GetKey(key) && CheckConditionals(conditions)) OnGunFired?.Invoke();
 				break;
 		}
 	}
@@ -44,6 +47,7 @@ public class GunParameters_SO : ScriptableObject
 	{
 		switch (ProjectileMethod)
 		{
+			default:
 			case BulletType.Hitscan:
 				hitscan?.Invoke();
 				break;
