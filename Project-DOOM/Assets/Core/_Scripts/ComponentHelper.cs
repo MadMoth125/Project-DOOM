bfe7b0a435c7ed78a1990f6c420f6f945b16d26f
MadMoth125 <madmoth125@gmail.com>
2023-10-23 14:57:29 -0500

    added a V6 to the Jobs billboard system, clean and concise structure
    reference V6 for any future use case where sprite switching and sprite billboards

---
+++ Project-DOOM/Assets/Core/_Scripts/ComponentHelper.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ComponentHelper
+{
+	public static T GetOrAddComponent<T>(this GameObject gameObject) where T : Component
+	{
+		T component = gameObject.GetComponent<T>();
+
+		return component == null ? gameObject.AddComponent<T>() : component;
+	}
+	
+	public static T SearchForComponent<T>(this GameObject gameObject) where T : Component
+	{
+		T component = gameObject.GetComponent<T>();
+
+		return component == null ? gameObject.GetComponentInChildren<T>() : component;
+	}
+}
---
+++ Project-DOOM/Assets/Core/_Scripts/Enemy/Enemy DOTS/V5/ISpriteComponentReferences.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ISpriteComponentReferences
+{
+	public void UpdateAnimatorIndex(int index);
+	
+	public void UpdateSpriteDirection(float yRotation);
+	
+	public Transform GetSpriteTransform();
+	
+	public Transform GetTransform();
+}
