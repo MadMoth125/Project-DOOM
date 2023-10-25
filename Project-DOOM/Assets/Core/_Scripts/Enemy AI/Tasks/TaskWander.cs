483d6fb8a454a4ab3013e7bb21be16df421494ff
MadMoth125 <madmoth125@gmail.com>
2023-10-25 14:57:58 -0500

    Update TaskWander.cs

--- Project-DOOM/Assets/Core/_Scripts/Enemy AI/Tasks/TaskWander.cs
+++ Project-DOOM/Assets/Core/_Scripts/Enemy AI/Tasks/TaskWander.cs
@@ -3,26 +3,54 @@ using System.Collections;
 using System.Collections.Generic;
 using MinaPechuex.BehaviorTree;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class TaskWander : Node
 {
-	private void OnEnable()
+	private NavMeshAgent _navAgentComponent;
+	
+	private Vector3 _wanderPivot;
+	private float _wanderRadius;
+	
+	private float _minWaitTime;
+	private float _maxWaitTime;
+
+	private bool _wating = true;
+	
+	public TaskWander(NavMeshAgent navReference, Vector3 wanderPivot, float wanderRadius)
 	{
+		_navAgentComponent = navReference;
 		
+		_wanderPivot = wanderPivot;
+		_wanderRadius = wanderRadius;
+		
+		_minWaitTime = UnityEngine.Random.Range(1f, 4f);
+		_maxWaitTime = UnityEngine.Random.Range(6f, 10f);
+	}
+	
+	public TaskWander(NavMeshAgent navReference, Vector3 wanderPivot, float wanderRadius, float minWaitTime, float maxWaitTime)
+	{
+		_navAgentComponent = navReference;
+		
+		_wanderPivot = wanderPivot;
+		_wanderRadius = wanderRadius;
+		
+		_minWaitTime = minWaitTime;
+		_maxWaitTime = maxWaitTime;
 	}
 
-	private void OnDisable()
+	public override NodeState Evaluate()
 	{
-		
-	}
+		_navAgentComponent.SetDestination(_wanderPivot);
+		if (_navAgentComponent.velocity != Vector3.zero)
+		{
+			
+		}
+		else
+		{
+			
+		}
 
-	private void Start()
-	{
-		
-	}
-
-	private void Update()
-	{
-		
+		return NodeState.RUNNING;
 	}
 }
